933b43ee04df8e489b4fada50ff7d785ac0d61eb
Oriol Gual <oriol.gual@gmail.com>
2012-05-27 10:11:55 +0200

    Refactor Hyperclient::Response

--- lib/hyperclient/response.rb
+++ lib/hyperclient/response.rb
@@ -1,20 +1,30 @@
 require 'hyperclient/discoverer'
 
 module Hyperclient
+  # Internal: This class is responsible for parsing a response from the API
+  # and exposing some methods to access its values.
+  #
+  # It is mainly used by Hyperclient::Resource.
   class Response
+    extend Forwardable
+
+    # Public: Delegate resources to the discoverer.
+    def_delegators :@discoverer, :resources
+
+    # Public: Initializes a Response.
+    #
+    # response - A Hash representing the response from the API.
     def initialize(response)
       @response = response
+      @discoverer = Discoverer.new(@response)
     end
 
-    def resources
-      discoverer = Discoverer.new(@response)
-      discoverer.resources
+    # Public: Returns a Hash with the attributes of the resource.
+    def attributes
+      @attributes ||= @response.delete_if {|key, value| key =~ /^_/}
     end
 
-    def data
-      @response.delete_if {|key, value| key =~ /^_/}
-    end
-
+    # Public: Returns a String with the resource URL.
     def url
       @response['_links']['self']['href']
     end
--- test/hyperclient/response_test.rb
+++ test/hyperclient/response_test.rb
@@ -1,5 +1,30 @@
 require_relative '../test_helper'
 require 'hyperclient/response'
 
-describe Hyperclient::Response do
+module Hyperclient
+  describe Response do
+    let (:response) do
+      Response.new JSON.parse(File.read('test/fixtures/element.json'))
+    end
+
+    describe 'attributes' do
+      it 'returns the resource attributes' do
+        response.attributes['title'].must_equal 'Real World ASP.NET MVC3'
+      end
+
+      it 'dos not include _links as attributes' do
+        response.attributes.wont_include '_links'
+      end
+
+      it 'does not inclide _embedded as attributes' do
+        response.attributes.wont_include '_embedded'
+      end
+    end
+
+    describe 'url' do
+      it 'returns the url of the resource grabbed from the response' do
+        response.url.must_equal '/productions/1'
+      end
+    end
+  end
 end
