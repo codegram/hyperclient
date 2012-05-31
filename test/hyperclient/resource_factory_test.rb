b0a4f93bbb4ce983a6b45d61e8aea44cf5df44f8
Oriol Gual <oriol.gual@gmail.com>
2012-05-31 16:20:41 +0200

    Simple Identity map

---
+++ test/hyperclient/resource_factory_test.rb
@@ -0,0 +1,28 @@
+require_relative '../test_helper'
+require 'hyperclient/resource_factory'
+
+module Hyperclient
+  describe ResourceFactory do
+    let(:resource) do
+      ResourceFactory.resource('/path/to/resource')
+    end
+
+    before do
+      Hyperclient::Resource.entry_point = 'http://myapi.org'
+    end
+
+    describe 'resource' do
+      it 'creates a new resource when is not present in the identity map' do
+        resource = ResourceFactory.resource('/path/to/resource')
+
+        resource.url.must_include '/path/to/resource'
+      end
+
+      it 'returns the resource when is already present in the identity map' do
+        new_resource = ResourceFactory.resource('/path/to/resource')
+
+        new_resource.object_id.must_equal resource.object_id
+      end
+    end
+  end
+end
