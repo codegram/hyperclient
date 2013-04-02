51b974c4894f96fcf903682611ce1bba4f13b973
rehevkor5 <shannon.carey@wrapports.com>
2013-04-02 14:26:20 -0500

    - Added support for arrays of Links in the _links area, as defined in HAL standard (_links => { name => [{href => ''}, {href => ''}]})

--- lib/hyperclient/link_collection.rb
+++ lib/hyperclient/link_collection.rb
@@ -13,13 +13,24 @@ module Hyperclient
     # Public: Initializes a LinkCollection.
     #
     # collection  - The Hash with the links.
-    # entry_point - The EntryPoint object to inject the cofnigutation.
+    # entry_point - The EntryPoint object to inject the configuration.
     #
     def initialize(collection, entry_point)
-      raise "Invalid response for LinkCollection. The response was: #{collection.inspect}" if collection && !collection.respond_to?(:inject)
-      @collection = (collection || {}).inject({}) do |hash, (name, link)|
-        hash.update(name => Link.new(link, entry_point))
-      end
+      raise "Invalid response for LinkCollection. The response was: #{collection.inspect}" if collection && !collection.respond_to?(:collect)
+
+      @collection = Hash[
+        (collection || {}).collect do |name, link_or_links|
+          value = if link_or_links.class == Array
+            link_or_links.collect do |link|
+              Link.new(link, entry_point)
+            end
+          else
+            Link.new(link_or_links, entry_point)
+          end
+
+          [name, value]
+        end
+      ]
     end
   end
 end
