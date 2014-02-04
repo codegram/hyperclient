fd46a9112d6653bb7064ffc1e396c037312996e8
Evgeny Dudin <nagliyvred@gmail.com>
2014-02-04 17:25:15 +1100

    Added an ability to override the default Faraday block

--- test/hyperclient/entry_point_test.rb
+++ test/hyperclient/entry_point_test.rb
@@ -23,6 +23,21 @@ module Hyperclient
         handlers.must_include FaradayMiddleware::ParseJson
         handlers.must_include Faraday::Adapter::NetHttp
       end
+
+      describe 'when initialized with a custom block' do
+        let(:entry_point) do
+          EntryPoint.new('http://my.api.org') do |connection|
+            connection.use FaradayMiddleware::FollowRedirects
+            connection.adapter :net_http
+          end
+        end
+
+        it 'creates a Faraday connection with that block' do
+          handlers = entry_point.connection.builder.handlers
+          handlers.must_include Faraday::Adapter::NetHttp
+          handlers.must_include FaradayMiddleware::FollowRedirects
+        end
+      end
     end
 
     describe 'initialize' do
