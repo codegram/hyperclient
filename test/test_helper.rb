ddd1b667fb3ccbf4226d82bc8f8feabd17501a29
Koen Punt <koen@koenpunt.nl>
2014-11-30 23:50:46 +0100

    replace webmock stubs with faraday stubs
    to correctly handle the serialized JSON

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,14 @@ require 'minitest/spec'
 require 'minitest/autorun'
 require 'mocha/setup'
 require 'turn'
-require 'webmock/minitest'
 require 'json'
 require 'pry'
+
+MiniTest::Unit::TestCase.class_eval do
+
+  def stub_request(conn, adapter_class = Faraday::Adapter::Test, &stubs_block)
+    adapter_handler = conn.builder.handlers.find { |h| h.klass < Faraday::Adapter }
+    conn.builder.swap(adapter_handler, adapter_class, &stubs_block)
+  end
+
+end
