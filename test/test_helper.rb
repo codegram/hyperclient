6ce8a9d5f78bfb671e980f942af9cd89288b307e
dblock <dblock@dblock.org>
2020-05-14 17:51:15 -0400

    Test against Faraday 0.9, 0.17 and 1.0+.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,11 @@ require 'json'
 
 MiniTest::Test.class_eval do
   def stub_request(conn, adapter_class = Faraday::Adapter::Test, &stubs_block)
-    conn.builder.adapter adapter_class, &stubs_block
+    adapter_handler = conn.builder.handlers.find { |h| h.klass < Faraday::Adapter }
+    if adapter_handler
+      conn.builder.swap(adapter_handler, adapter_class, &stubs_block)
+    else
+      conn.builder.adapter adapter_class, &stubs_block
+    end
   end
 end
